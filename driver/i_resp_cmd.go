e1b4496620409e9c70d7ba5721d54313aba0e59f
weedge <weege007@gmail.com>
2023-06-02 13:13:13 +0800

    add ctx

--- driver/i_resp_cmd.go
+++ driver/i_resp_cmd.go
@@ -2,6 +2,7 @@
 package driver
 
 import (
+	"context"
 	"time"
 
 	openkvdriver "github.com/weedge/pkg/driver/openkv"
@@ -14,41 +15,47 @@ type KVPair struct {
 
 // adapt https://redis.io/commands/?group=string
 type IStringCmd interface {
-	Set(key []byte, value []byte) error
-	Get(key []byte) ([]byte, error)
-	GetSlice(key []byte) (openkvdriver.ISlice, error)
-	GetSet(key []byte, value []byte) ([]byte, error)
-	Incr(key []byte) (int64, error)
-	IncrBy(key []byte, increment int64) (int64, error)
-	Decr(key []byte) (int64, error)
-	DecrBy(key []byte, decrement int64) (int64, error)
-	MGet(keys ...[]byte) ([][]byte, error)
-	MSet(args ...KVPair) error
-	SetNX(key []byte, value []byte) (n int64, err error)
-	SetEX(key []byte, duration int64, value []byte) error
-	SetRange(key []byte, offset int, value []byte) (int64, error)
-	GetRange(key []byte, start int, end int) ([]byte, error)
-	StrLen(key []byte) (int64, error)
-	Append(key []byte, value []byte) (int64, error)
+	Set(ctx context.Context, key []byte, value []byte) error
+	SetNX(ctx context.Context, key []byte, value []byte) (n int64, err error)
+	SetEX(ctx context.Context, key []byte, duration int64, value []byte) error
+	SetNXEX(ctx context.Context, key []byte, duration int64, value []byte) error
+
+	Get(ctx context.Context, key []byte) ([]byte, error)
+	GetSlice(ctx context.Context, key []byte) (openkvdriver.ISlice, error)
+	GetSet(ctx context.Context, key []byte, value []byte) ([]byte, error)
+
+	Incr(ctx context.Context, key []byte) (int64, error)
+	IncrBy(ctx context.Context, key []byte, increment int64) (int64, error)
+	Decr(ctx context.Context, key []byte) (int64, error)
+	DecrBy(ctx context.Context, key []byte, decrement int64) (int64, error)
+
+	MGet(ctx context.Context, keys ...[]byte) ([][]byte, error)
+	MSet(ctx context.Context, args ...KVPair) error
+
+	SetRange(ctx context.Context, key []byte, offset int, value []byte) (int64, error)
+	GetRange(ctx context.Context, key []byte, start int, end int) ([]byte, error)
+
+	StrLen(ctx context.Context, key []byte) (int64, error)
+	Append(ctx context.Context, key []byte, value []byte) (int64, error)
 
 	ICommonCmd
 }
 
 // adapt https://redis.io/commands/?group=list
 type IListCmd interface {
-	LIndex(key []byte, index int32) ([]byte, error)
-	LLen(key []byte) (int64, error)
-	LPop(key []byte) ([]byte, error)
-	LTrim(key []byte, start, stop int64) error
-	LTrimFront(key []byte, trimSize int32) (int32, error)
-	LTrimBack(key []byte, trimSize int32) (int32, error)
-	LPush(key []byte, args ...[]byte) (int64, error)
-	LSet(key []byte, index int32, value []byte) error
-	LRange(key []byte, start int32, stop int32) ([][]byte, error)
-	RPop(key []byte) ([]byte, error)
-	RPush(key []byte, args ...[]byte) (int64, error)
-	BLPop(keys [][]byte, timeout time.Duration) ([]interface{}, error)
-	BRPop(keys [][]byte, timeout time.Duration) ([]interface{}, error)
+	LIndex(ctx context.Context, key []byte, index int32) ([]byte, error)
+	LLen(ctx context.Context, key []byte) (int64, error)
+	LPop(ctx context.Context, key []byte) ([]byte, error)
+	LTrim(ctx context.Context, key []byte, start, stop int64) error
+	LTrimFront(ctx context.Context, key []byte, trimSize int32) (int32, error)
+	LTrimBack(ctx context.Context, key []byte, trimSize int32) (int32, error)
+	LPush(ctx context.Context, key []byte, args ...[]byte) (int64, error)
+	LSet(ctx context.Context, key []byte, index int32, value []byte) error
+	LRange(ctx context.Context, key []byte, start int32, stop int32) ([][]byte, error)
+	RPop(ctx context.Context, key []byte) ([]byte, error)
+	RPush(ctx context.Context, key []byte, args ...[]byte) (int64, error)
+	BLPop(ctx context.Context, keys [][]byte, timeout time.Duration) ([]interface{}, error)
+	BRPop(ctx context.Context, keys [][]byte, timeout time.Duration) ([]interface{}, error)
 
 	ICommonCmd
 }
@@ -60,32 +67,32 @@ type FVPair struct {
 
 // adapt https://redis.io/commands/?group=hash
 type IHashCmd interface {
-	HSet(key []byte, field []byte, value []byte) (int64, error)
-	HGet(key []byte, field []byte) ([]byte, error)
-	HLen(key []byte) (int64, error)
-	HMset(key []byte, args ...FVPair) error
-	HMget(key []byte, args ...[]byte) ([][]byte, error)
-	HDel(key []byte, args ...[]byte) (int64, error)
-	HIncrBy(key []byte, field []byte, delta int64) (int64, error)
-	HGetAll(key []byte) ([]FVPair, error)
-	HKeys(key []byte) ([][]byte, error)
-	HValues(key []byte) ([][]byte, error)
+	HSet(ctx context.Context, key []byte, field []byte, value []byte) (int64, error)
+	HGet(ctx context.Context, key []byte, field []byte) ([]byte, error)
+	HLen(ctx context.Context, key []byte) (int64, error)
+	HMset(ctx context.Context, key []byte, args ...FVPair) error
+	HMget(ctx context.Context, key []byte, args ...[]byte) ([][]byte, error)
+	HDel(ctx context.Context, key []byte, args ...[]byte) (int64, error)
+	HIncrBy(ctx context.Context, key []byte, field []byte, delta int64) (int64, error)
+	HGetAll(ctx context.Context, key []byte) ([]FVPair, error)
+	HKeys(ctx context.Context, key []byte) ([][]byte, error)
+	HValues(ctx context.Context, key []byte) ([][]byte, error)
 
 	ICommonCmd
 }
 
 // adapt https://redis.io/commands/?group=set
 type ISetCmd interface {
-	SAdd(key []byte, args ...[]byte) (int64, error)
-	SCard(key []byte) (int64, error)
-	SDiff(keys ...[]byte) ([][]byte, error)
+	SAdd(ctx context.Context, key []byte, args ...[]byte) (int64, error)
+	SCard(ctx context.Context, key []byte) (int64, error)
+	SDiff(ctx context.Context, keys ...[]byte) ([][]byte, error)
 	SDiffStore(dstKey []byte, keys ...[]byte) (int64, error)
-	SInter(keys ...[]byte) ([][]byte, error)
+	SInter(ctx context.Context, keys ...[]byte) ([][]byte, error)
 	SInterStore(dstKey []byte, keys ...[]byte) (int64, error)
-	SIsMember(key []byte, member []byte) (int64, error)
-	SMembers(key []byte) ([][]byte, error)
-	SRem(key []byte, args ...[]byte) (int64, error)
-	SUnion(keys ...[]byte) ([][]byte, error)
+	SIsMember(ctx context.Context, key []byte, member []byte) (int64, error)
+	SMembers(ctx context.Context, key []byte) ([][]byte, error)
+	SRem(ctx context.Context, key []byte, args ...[]byte) (int64, error)
+	SUnion(ctx context.Context, keys ...[]byte) ([][]byte, error)
 	SUnionStore(dstKey []byte, keys ...[]byte) (int64, error)
 
 	ICommonCmd
@@ -113,36 +120,36 @@ const (
 
 // adapt https://redis.io/commands/?group=sorted-set
 type IZsetCmd interface {
-	ZAdd(key []byte, args ...ScorePair) (int64, error)
-	ZCard(key []byte) (int64, error)
-	ZScore(key []byte, member []byte) (int64, error)
-	ZRem(key []byte, members ...[]byte) (int64, error)
-	ZIncrBy(key []byte, delta int64, member []byte) (int64, error)
-	ZCount(key []byte, min int64, max int64) (int64, error)
-	ZRank(key []byte, member []byte) (int64, error)
-	ZRemRangeByRank(key []byte, start int, stop int) (int64, error)
-	ZRemRangeByScore(key []byte, min int64, max int64) (int64, error)
-	ZRevRange(key []byte, start int, stop int) ([]ScorePair, error)
-	ZRevRank(key []byte, member []byte) (int64, error)
-	ZRevRangeByScore(key []byte, min int64, max int64, offset int, count int) ([]ScorePair, error)
-	ZRangeGeneric(key []byte, start int, stop int, reverse bool) ([]ScorePair, error)
-	ZRangeByScoreGeneric(key []byte, min int64, max int64, offset int, count int, reverse bool) ([]ScorePair, error)
+	ZAdd(ctx context.Context, key []byte, args ...ScorePair) (int64, error)
+	ZCard(ctx context.Context, key []byte) (int64, error)
+	ZScore(ctx context.Context, key []byte, member []byte) (int64, error)
+	ZRem(ctx context.Context, key []byte, members ...[]byte) (int64, error)
+	ZIncrBy(ctx context.Context, key []byte, delta int64, member []byte) (int64, error)
+	ZCount(ctx context.Context, key []byte, min int64, max int64) (int64, error)
+	ZRank(ctx context.Context, key []byte, member []byte) (int64, error)
+	ZRemRangeByRank(ctx context.Context, key []byte, start int, stop int) (int64, error)
+	ZRemRangeByScore(ctx context.Context, key []byte, min int64, max int64) (int64, error)
+	ZRevRange(ctx context.Context, key []byte, start int, stop int) ([]ScorePair, error)
+	ZRevRank(ctx context.Context, key []byte, member []byte) (int64, error)
+	ZRevRangeByScore(ctx context.Context, key []byte, min int64, max int64, offset int, count int) ([]ScorePair, error)
+	ZRangeGeneric(ctx context.Context, key []byte, start int, stop int, reverse bool) ([]ScorePair, error)
+	ZRangeByScoreGeneric(ctx context.Context, key []byte, min int64, max int64, offset int, count int, reverse bool) ([]ScorePair, error)
 	ZUnionStore(destKey []byte, srcKeys [][]byte, weights []int64, aggregate []byte) (int64, error)
 	ZInterStore(destKey []byte, srcKeys [][]byte, weights []int64, aggregate []byte) (int64, error)
-	ZRangeByLex(key []byte, min []byte, max []byte, rangeType RangeType, offset int, count int) ([][]byte, error)
-	ZRemRangeByLex(key []byte, min []byte, max []byte, rangeType RangeType) (int64, error)
-	ZLexCount(key []byte, min []byte, max []byte, rangeType RangeType) (int64, error)
+	ZRangeByLex(ctx context.Context, key []byte, min []byte, max []byte, rangeType RangeType, offset int, count int) ([][]byte, error)
+	ZRemRangeByLex(ctx context.Context, key []byte, min []byte, max []byte, rangeType RangeType) (int64, error)
+	ZLexCount(ctx context.Context, key []byte, min []byte, max []byte, rangeType RangeType) (int64, error)
 
 	ICommonCmd
 }
 
 // adapt https://redis.io/commands/?group=bitmap
 type IBitmapCmd interface {
-	BitOP(op string, destKey []byte, srcKeys ...[]byte) (int64, error)
-	BitCount(key []byte, start int, end int) (int64, error)
-	BitPos(key []byte, on int, start int, end int) (int64, error)
-	SetBit(key []byte, offset int, on int) (int64, error)
-	GetBit(key []byte, offset int) (int64, error)
+	BitOP(ctx context.Context, op string, destKey []byte, srcKeys ...[]byte) (int64, error)
+	BitCount(ctx context.Context, key []byte, start int, end int) (int64, error)
+	BitPos(ctx context.Context, key []byte, on int, start int, end int) (int64, error)
+	SetBit(ctx context.Context, key []byte, offset int, on int) (int64, error)
+	GetBit(ctx context.Context, key []byte, offset int) (int64, error)
 
 	ICommonCmd
 }
@@ -150,17 +157,17 @@ type IBitmapCmd interface {
 // adapt https://redis.io/commands/?group=generic
 // some common key op cmd
 type ICommonCmd interface {
-	Del(keys ...[]byte) (int64, error)
-	Exists(key []byte) (int64, error)
+	Del(ctx context.Context, keys ...[]byte) (int64, error)
+	Exists(ctx context.Context, key []byte) (int64, error)
 	// for ttl
-	Expire(key []byte, duration int64) (int64, error)
-	ExpireAt(key []byte, when int64) (int64, error)
-	TTL(key []byte) (int64, error)
-	Persist(key []byte) (int64, error)
+	Expire(ctx context.Context, key []byte, duration int64) (int64, error)
+	ExpireAt(ctx context.Context, key []byte, when int64) (int64, error)
+	TTL(ctx context.Context, key []byte) (int64, error)
+	Persist(ctx context.Context, key []byte) (int64, error)
 }
 
 type IDB interface {
-	FlushDB() (drop int64, err error)
+	FlushDB(ctx context.Context) (drop int64, err error)
 
 	DBString() IStringCmd
 	DBList() IListCmd
@@ -171,7 +178,7 @@ type IDB interface {
 }
 
 type IStorager interface {
-	Select(index int) (db IDB, err error)
+	Select(ctx context.Context, index int) (db IDB, err error)
+	FlushAll(ctx context.Context) error
 	Close() error
-	FlushAll() error
 }
